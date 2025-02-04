20f81205cc3b3c7bbd4fccd28b69b1c491a1a1f1
Alin Cojocaru <cojocaru.alin@alsodev.md>
2025-02-04 14:50:03 +0200

    Timezone timestamp fixed

--- models/cell_sensors.py
+++ models/cell_sensors.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.types import BigInteger, DECIMAL
 
 from utilities.database.models import Base
+from utilities.database.types import TZDateTime
 
 
 class SensorRecord(Base):
@@ -11,4 +12,4 @@ class SensorRecord(Base):
     device_id: Mapped[int] = mapped_column(BigInteger, index=True, nullable=False)
     is_in_cell: Mapped[bool] = mapped_column(nullable=False)
     dwell_time = mapped_column(DECIMAL, nullable=True)
-    timestamp: Mapped[datetime] = mapped_column(nullable=False)
+    timestamp: Mapped[datetime] = mapped_column(TZDateTime, nullable=False)
---
+++ utilities/database/types.py
@@ -0,0 +1,19 @@
+from datetime import timezone
+from sqlalchemy.types import DateTime, TypeDecorator
+
+
+class TZDateTime(TypeDecorator):
+    impl = DateTime
+    cache_ok = True
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            if not value.tzinfo or value.tzinfo.utcoffset(value) is None:
+                return value
+            value = value.astimezone(timezone.utc).replace(tzinfo=None)
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            value = value.replace(tzinfo=timezone.utc)
+        return value
